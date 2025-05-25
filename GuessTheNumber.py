6fc4fbbc7378364929a8b9c9365f8fefd7f2fd27
Basavanagouda2006 <basava9221@gmail.com>
2025-05-25 19:17:55 +0530

    Add files via upload

---
+++ GuessTheNumber.py
@@ -0,0 +1,8 @@
+print('NAME:basavanagouda.y' ,'USN:1AY24AI017')
+import random
+number=random.randint(1,100)
+guess=int(input("guess the number between 1 and 100:"))
+if guess==number:
+    print("you guessed it!!")
+else:
+    print(f"wrong! the number was {number}")
